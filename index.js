dd796501f42d4f98a2af754a1680433226ae77ef
Delsin <devagr@hotmail.com>
2021-04-24 10:05:27 +0200

    Skelet hreflang check

---
+++ index.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require("axios").default;
+
+try {
+    const siteUrl = core.getInput("site");
+
+    axios
+    .get(siteUrl)
+    .then(function (response) {
+      let hreflang = response.data.match(
+        /<link rel="alternate" ([^<]+)>/
+      );
+
+      console.log(hreflang)
+
+      if (! favicon) {
+        core.setFailed("No hreflang tag on page");
+      }
+
+    //   if (favicon) {
+    //     let faviconUrl = getHref(favicon[0])
+    //     core.setOutput("favicon", faviconUrl);
+    //   }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+function getHref(href) {
+    let start_pos = href.indexOf("href=\"") + 6;
+    let end_pos = href.indexOf("\"", start_pos);
+    return resultHref = href.substring(start_pos, end_pos);
+  }
