ec512b1f6ed814cfdb0b77ce1b0d2d5c59e33453
Delsin <devagr@hotmail.com>
2021-04-25 10:00:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require("axios").default;
 
 try {
     const siteUrl = core.getInput("site");
-    let regex = /<link rel="alternate" href="([^")]*)" hreflang="([^")]*)">/g
+    let regex1 = /<link rel="alternate" ([^")]*)>/g
     axios
     .get(siteUrl)
     .then(function (response) {
